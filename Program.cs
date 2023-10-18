8121072cf1a13087a58a7f1b13eee3a5d70082b6
Roman_Batrakov <kyct69@yandex.ru>
2023-10-18 23:21:09 +0300

    Разработал программу

--- Program.cs
+++ Program.cs
@@ -7,4 +7,54 @@
                                                                                                  
 /////////////////////////////////////////////////////////////////////////////////////////////////*/
 
-string[] words = new string[0];
+// Метод вывода массива
+void PrintArray(string[] array, string message)
+{
+Console.WriteLine("\n" + message + "");
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+    {
+       Console.Write($"\"{array[i]}\"");
+       if (i < array.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        
+    }
+Console.Write("]\n");
+}
+
+// Метод формирования нового массива из строк, длина которых меньше, либо равна 3 символам
+string[] NewArrayThreeSym(string[] array)
+{
+int count = 0;    
+string[] arrayNew = new string[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) 
+        {
+            arrayNew[count] = array[i];
+            count ++;     
+        }
+        
+    }
+if (arrayNew.Length > count + 1) {Array.Resize(ref arrayNew, count);}
+return arrayNew;  
+}
+
+
+
+string[] arrayFirst = {"Hello", "2", "world", ":-)"};
+string[] arraySecond = {"1234", "1567", "-2", "computer science"};
+string[] arrayThird = {"Russia", "Denmark", "Kazan"};
+
+PrintArray(arrayFirst, "Первый массив:");
+PrintArray(NewArrayThreeSym(arrayFirst), "Новый массив:");
+
+PrintArray(arraySecond, "Второй массив:");
+PrintArray(NewArrayThreeSym(arraySecond), "Новый массив:");
+
+PrintArray(arrayThird, "Третий массив:");
+PrintArray(NewArrayThreeSym(arrayThird), "Новый массив:");
+Console.ReadKey();
